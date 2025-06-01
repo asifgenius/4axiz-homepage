6178e050ecc32e85331f2e00cd844d415774bec8
Asif Islam <engrasifislam@gmail.com>
2025-06-01 22:48:43 +0600

    Add hero file

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,68 @@
+<div class="uwy userway_p5" data-uw-feature-ignore="true" data-uw-rm-ignore="true" title="Accessibility Menu"
+    style="background-color: transparent !important; overflow: initial !important;">
+    <div class="userway_buttons_wrapper">
+        <div class="ulsti hidden uli" id="userwayLstIcon" aria-label="Translations Menu" role="button" tabindex="0"
+            aria-haspopup="dialog" data-uw-s19-ignore="" title="Translations Menu"
+            style="background: rgb(131, 183, 53) !important;"><span class="uiiw"></span>
+            <div class="ups"><img width="43" height="43" data-uw-rm-ignore="" class="si_b" aria-hidden="true"
+                    alt="Spinner: Black decorative" src="assets/spin_bl.svg"></div><span
+                class="usr lst-spacer"></span><img class="userway_check_on" data-uw-rm-ignore=""
+                data-uw-ignore-s25="true" width="18" height="20" alt="Accessibility menu is on"
+                src="assets/check_on.svg">
+        </div>
+        <div class="uai uli" id="userwayAccessibilityIcon" aria-label="Accessibility Menu" role="button" tabindex="0"
+            aria-haspopup="dialog" title="Accessibility Menu" style="background: rgb(131, 183, 53) !important;"><span
+                class="uiiw"><img data-uw-rm-ignore="" class="ui_b" role="presentation" alt=""
+                    src="assets/wheel_right_bl.svg"></span>
+            <div class="ups"><img width="43" height="43" data-uw-rm-ignore="" class="si_b" aria-hidden="true"
+                    alt="Spinner: Black decorative" src="assets/spin_bl.svg"></div><span class="usr"></span><img
+                class="userway_check_on" data-uw-rm-ignore="" data-uw-ignore-s25="true" width="18" height="20"
+                alt="Accessibility menu is on" src="assets/check_on.svg">
+        </div>
+        <div class="uwaw-dictionary-tooltip"></div>
+    </div><iframe class="uwif userway_p5" data-uw-ignore-translate="true" name="userway"
+        title="UserWay Accessibility Menu"
+        style="max-width: 100vw !important; visibility: visible !important; opacity: 1 !important; color-scheme: light !important;"
+        src="js/saved_resource(1).html"></iframe>
+</div>
+<div class="uw-s10-bottom-ruler-guide"></div>
+<div class="uw-s10-right-ruler-guide"></div>
+<div class="uw-s10-left-ruler-guide"></div>
+<div class="uw-s10-reading-guide">
+    <div class="uw-s10-reading-guide__arrow"></div>
+</div>
+<div class="uw-s12-tooltip" aria-hidden="true"></div><noscript><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-52DTNJNT" height="0" width="0"
+        style="display:none;visibility:hidden" aria-hidden="true"></iframe></noscript>
+
+<div class="side-panel">
+    <div class="close-black-block-offcanvas"><a href="https://fouraxiz.com/#"><i class="fa fa-times"></i></a></div>
+</div>
+
+<div id="myCarousel" class="carousel slide blank-builder" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+
+
+            <img src="//fouraxiz.com/wp-content/uploads/2023/08/app_banner.jpg" class="d-block w-100" alt="Image 1">
+        </div>
+        <div class="carousel-item">
+            <img src="//fouraxiz.com/wp-content/uploads/2023/08/ai-banner.jpg" class="d-block w-100" alt="Image 2">
+        </div>
+        <div class="carousel-item">
+            <img src="//fouraxiz.com/wp-content/uploads/2023/08/banner-one-1.jpg" class="d-block w-100" alt="Image 3">
+        </div>
+        <div class="carousel-item">
+            <img src="//fouraxiz.com/wp-content/uploads/2023/08/web_banner.jpg" class="d-block w-100" alt="Image 3">
+        </div>
+        <div class="carousel-item">
+            <img src="//fouraxiz.com/wp-content/uploads/2023/08/banner-two-1.jpg" class="d-block w-100" alt="Image 3">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </button>
+</div>
